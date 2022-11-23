d10fa8699d7fc54a7331a3fdafb82a870b96314f
stiwari2022 <86172530+stiwari2022@users.noreply.github.com>
2022-11-22 21:10:36 -0500

    Update Parts_Of_Speech_Identifier.py

--- Iambic Pentameters/Parts_Of_Speech_Identifier.py
+++ Iambic Pentameters/Parts_Of_Speech_Identifier.py
@@ -1,9 +1,9 @@
 def Parts_Of_Speech(Word):
   import nltk
   from nltk import pos_tag, word_tokenize
-  ###nltk.download('punkt')
-  ###nltk.download('universal_tagset')
-  ###nltk.download('averaged_perceptron_tagger')
+  #nltk.download('punkt')
+  #nltk.download('universal_tagset')
+  #nltk.download('averaged_perceptron_tagger')
   Tokenized_Word=pos_tag(word_tokenize(Word), tagset='universal')
   Part_Of_Speech=Tokenized_Word[0][1]
   return Part_Of_Speech
