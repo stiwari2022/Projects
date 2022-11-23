e487afaf144f759d205aa9b6498e49207a9a9318
stiwari2022 <86172530+stiwari2022@users.noreply.github.com>
2022-11-23 10:08:43 -0500

    Update Parts_Of_Speech_Identifier.py

--- Iambic Pentameters/Parts_Of_Speech_Identifier.py
+++ Iambic Pentameters/Parts_Of_Speech_Identifier.py
@@ -8,3 +8,14 @@ def Simplified_Parts_Of_Speech(Word):
   Part_Of_Speech=Tokenized_Word[0][1]
   return Part_Of_Speech
 Simplified_Parts_Of_Speech("and")
+
+
+
+def Complicated_Parts_Of_Speech(Word):
+  import nltk
+  from nltk import pos_tag, word_tokenize
+  #nltk.download('punkt')
+  Tokenized_Word=pos_tag(word_tokenize(Word))
+  Part_Of_Speech=Tokenized_Word[0][1]
+  return Part_Of_Speech
+Complicated_Parts_Of_Speech("and")
